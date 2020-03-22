0b6470187717d27f89ec45d59e074c52e2a61b1e
mads2 <mads2k5@gmail.com>
2020-03-22 17:37:16 -0300

    website

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# EasyDictionay
-A quick bookmark shortcut to the great Cambridge English Dictionary
+# EasyDictionary
+### A quick bookmark shortcut to the great Cambridge English Dictionary
+
+How to install:
+* Simple drag the link in [mads2.github.io/EasyDictionary](https://mads2.github.io/EasyDictionary/) to your bookmarks bar.
+
---
+++ source/easyDictionaryMini.js
@@ -0,0 +1,1 @@
+javascript:(function(){var getSelectedText=function(){var text='';if(window.getSelection){text=window.getSelection().toString()}else if(document.selection&&document.selection.type!='Control'){text=document.selection.createRange().text} return text};window.open('https://dictionary.cambridge.org/dictionary/english/'+getSelectedText())})()
